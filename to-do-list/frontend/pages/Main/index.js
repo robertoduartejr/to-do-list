25c4b43063dbe0dd5fbb374aa09f2f6db7daca8e
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-11-24 12:20:50 -0300

    pending function to delete each task

--- to-do-list/frontend/pages/Main/index.js
+++ to-do-list/frontend/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 
@@ -9,38 +9,48 @@ const Main = () => {
   const [todoList, setTodoList] = useState([])
 
   const HandleAddClick = () => {
-    setTodoList([...todoList,inputValue])
+    setTodoList([...todoList, inputValue])
     setInputValue("")
+  }
+
+  const HandleClearClick = () => {
+    setTodoList([])
   }
 
   return (
     <div className="principal">
-    <div className="container2">
-      <b><h1 className='title'>TO-DO LIST</h1></b>
-      <div className="input-group mb-3">
-        <input 
-        onChange={(event) => setInputValue(event.target.value)} 
-        value={inputValue} 
-        className='form-control' 
-        type="text" 
-        placeholder='Add your new task'/>
+      <div className="container2">
+        <b><h1 className='title'>TO-DO LIST</h1></b>
+        <div className="input-group mb-3">
+          <input
+            onChange={(event) => setInputValue(event.target.value)}
+            value={inputValue}
+            className='form-control'
+            type="text"
+            placeholder='Add your new task' />
 
-        <button onClick={HandleAddClick} className='input-group-text btn btn-primary rounded-3'>+</button>
+          <button onClick={HandleAddClick} className='input-group-text btn btn-primary rounded-3'>+</button>
+
+        </div>
+
+        <div class="tasks">
+          <ul className='list-group'>
+            {
+              todoList.map((task) => <li className='list-group-item post'>{task}<button class="btn-del options">✔</button></li>)
+            }
+          </ul>
+        </div>
+        <div class="todo-footer">
+            You have {todoList.length} pending tasks
+            <button onClick={HandleClearClick} class="input-group-text btn btn-secondary rounded-"> Clear All</button>
+
+
+        </div>
+
+
+
 
       </div>
-
-      <div class="tasks">
-        <ul className='list-group'>
-      {
-          todoList.map((task) => <li className='list-group-item post'>{task}<button class="btn-del options">✔</button></li>)
-        } 
-        </ul>
-        </div>
-        
-
-
-
-    </div>
     </div>
   );
 }
