de9801f8dfb5d302d5f734967c2a18f31974e608
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-11-24 01:45:46 -0300

    finished del button

--- to-do-list/frontend/pages/Main/index.js
+++ to-do-list/frontend/pages/Main/index.js
@@ -5,21 +5,41 @@ import './styles.css';
 
 const Main = () => {
 
-  const [title, setTitle] = useState("To do List")
+  const [inputValue, setInputValue] = useState("")
   const [todoList, setTodoList] = useState([])
+
+  const HandleAddClick = () => {
+    setTodoList([...todoList,inputValue])
+    setInputValue("")
+  }
 
   return (
     <div className="principal">
     <div className="container2">
-      <b><h1 className='title'>TO-DO LIST - {title}</h1></b>
+      <b><h1 className='title'>TO-DO LIST</h1></b>
       <div className="input-group mb-3">
-        <input onChange={(event) => setTitle(event.target.value)} value={title} className='form-control' type="text" placeholder='Add your new task'/>
-        <button className='input-group-text btn btn-primary rounded-3'>+</button>
-        {
+        <input 
+        onChange={(event) => setInputValue(event.target.value)} 
+        value={inputValue} 
+        className='form-control' 
+        type="text" 
+        placeholder='Add your new task'/>
 
-        }
+        <button onClick={HandleAddClick} className='input-group-text btn btn-primary rounded-3'>+</button>
 
       </div>
+
+      <div class="tasks">
+        <ul className='list-group'>
+      {
+          todoList.map((task) => <li className='list-group-item post'>{task}<button class="btn-del options">✔</button></li>)
+        } 
+        </ul>
+        </div>
+        
+
+
+
     </div>
     </div>
   );
