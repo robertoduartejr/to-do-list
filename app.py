16bf3d93de683b90e43005fe9d65328cb88beda8
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-11-30 02:37:36 -0300

    URL confirmation ongoing

--- app.py
+++ app.py
@@ -4,19 +4,25 @@ from dotenv import load_dotenv
 import os
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-
+from flask_mail import Mail
+from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__, template_folder='./to-do-list/templates')
 app.app_context().push() #chamada para evitar working outside of application context
 SECRET_KEY = os.urandom(32) #creating random secret key to csrf
 app.config['SECRET_KEY'] = SECRET_KEY
+
 bootstrap = Bootstrap(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login' #here's where I define which page go when not logged in
 
+app.config.from_pyfile('config.cfg') #for the email
+Mail(app)
 
-
+SECRET_KEY_2 = os.urandom(32) #for the URL
+s = URLSafeTimedSerializer(SECRET_KEY_2)
 
 load_dotenv()
 
--- views.py
+++ views.py
@@ -1,11 +1,11 @@
 import flask
 from flask import render_template, request, url_for, redirect, session, flash
-from app import app, db, login_manager
+from app import app, db, login_manager,s
 from models import User
 from forms import LoginForm, RegisterForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
-
+from itsdangerous import SignatureExpired
 
 @login_manager.user_loader #handling with user login
 def load_user(user_id):
@@ -63,13 +63,26 @@ def register():
             new_user = User(name=form.name.data, email=form.email.data,password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
-            return redirect(url_for('index'))
+
+            email = form.email.data
+            token = s.dumps(email, salt='email-confirm')
+
+            return '<h1> The token is {}</h1>'.format(token)
+            #return redirect(url_for('index'))
         except:
             flash("We had a problem when saving information. Please try again", "internalerror")
             return redirect(url_for('index'))
 
     form = RegisterForm(request.form)
     return render_template('signup.html',form=form)
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt='email-confirm',max_age=120)
+    except SignatureExpired:
+        return redirect(url_for('index')) #colocar flash que expirou
+    return "the token works" #colocar flash que funcionou
 
 
 @app.route('/logout')
@@ -86,9 +99,28 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"), 500
 
-@app.route('/users')
+@app.route('/users') #function to send user information to react
 @login_required
 def users():
     return {"name": current_user.name.split(" ")[0], "tasks": current_user.tasks}
 
 
+@app.route('/delete/<int:id>') #simples function to delete users on interface
+@login_required
+def delete(id):
+    if current_user.email=="admin@admin.com":
+        user = User.query.get(id)
+        db.session.delete(user)
+        db.session.commit()
+        return redirect(url_for('specialadmin'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/specialadmin') #function to display all users
+@login_required
+def specialadmin():
+    if current_user.email == "admin@admin.com":
+        users = User.query.all()
+        return render_template("admin.html", users=users)
+    else:
+        return redirect(url_for('index'))
